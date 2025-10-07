e331e276927d84b5abe9d9e494481d3ee7f6c5ff
Alerrx20 <alejandro.real.ramos@gmail.com>
2025-10-07 13:04:28 +0100

    feat: add cube rotation animation for user interaction

--- src/app/components/cube/cube.ts
+++ src/app/components/cube/cube.ts
@@ -1,10 +1,44 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Signal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-cube',
   imports: [],
   templateUrl: './cube.html',
   styleUrl: './cube.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Cube {}
+export class Cube implements OnInit {
+  isDraggin = signal(false);
+  startX!: Signal<number>;
+  startY!: Signal<number>;
+  currentX = signal(0);
+  currentY = signal(0);
+
+  ngOnInit() {
+    const cube = document.querySelector('.cube') as HTMLElement;
+
+    cube.addEventListener('mousedown', (event) => {
+      this.isDraggin.set(true);
+      this.startX = signal(event.clientX);
+      this.startY = signal(event.clientY);
+      cube.style.cursor = 'grabbing';
+    });
+
+    cube.addEventListener('mousemove', (event) => {
+      if (this.isDraggin()) {
+        const deltaX = event.clientX - this.startX();
+        const deltaY = event.clientY - this.startY();
+        this.currentX.update((x) => x + deltaX);
+        this.currentY.update((y) => y - deltaY);
+        cube.style.transform = `rotateX(${this.currentY()}deg) rotateY(${this.currentX()}deg)`;
+        this.startX = signal(event.clientX);
+        this.startY = signal(event.clientY);
+      }
+    });
+
+    cube.addEventListener('mouseup', () => {
+      this.isDraggin.set(false);
+      cube.style.cursor = 'grab';
+    });
+  }
+}
